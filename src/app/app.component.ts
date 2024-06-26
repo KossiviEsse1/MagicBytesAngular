71b0e32d03dfa3ea197939be2a6e8c129a1ba2ae
KossiviEsse1 <kossivi14@gmail.com>
2024-06-25 19:46:44 -0500

    Updated Menu for Mobile purposes/small screens, made site responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, MatMenuModule, MatButtonModule],
   template: `
   <main>
     <header class="brand-name">
       <span>
         <h1 class="left"><a [routerLink]="['/']"> XOR Decrypter </a></h1>
-        <button class="primary right" [routerLink]="['/example']">Example</button>
-        <button class="primary right" [routerLink]="['/encrypter']">Encrypter</button>
-        <button class="primary right" [routerLink]="['/decrypter']">Decrypter</button>
+        <section class="navbarMenu">
+          <button class="primary right" [matMenuTriggerFor]="menu" mat-button>Menu</button>
+          <mat-menu #menu="matMenu">
+            <button mat-menu-item [routerLink]="['/']">Home</button>
+            <button mat-menu-item [routerLink]="['/decrypter']">Decrypter</button>
+            <button mat-menu-item [routerLink]="['/encrypter']">Encrypter</button>
+            <button mat-menu-item [routerLink]="['/example']">Example</button>
+          </mat-menu>
+        </section>
+        <section class="navbar">
+          <button class="primary right" [routerLink]="['/example']">Example</button>
+          <button class="primary right" [routerLink]="['/encrypter']">Encrypter</button>
+          <button class="primary right" [routerLink]="['/decrypter']">Decrypter</button>
+        </section>
       </span>
     </header>
     <section class="content">
