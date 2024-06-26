5d2e09436d4fb0eb36bf3472dabc58535c7230a9
KossiviEsse1 <kossivi14@gmail.com>
2024-06-26 17:40:34 -0500

    did some image load stuff, not complete

--- src/app/thumbnail/thumbnail.component.ts
+++ src/app/thumbnail/thumbnail.component.ts
@@ -2,13 +2,16 @@ import { Component, Input } from '@angular/core';
 import { ThumbnailDetails } from '../thumbnail-details';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-thumbnail',
   standalone: true,
   imports: [CommonModule],
   template: `
     <section class="card">
-      <img class="listing-photo" [src]="thumbnailDetails.photo" alt="image1" style="width: 100%">
+      <section class="image">
+        <img class="listing-photo" [src]="thumbnailDetails.photo" alt="image1" style="width: 100%">
+      </section>
       <section class="container">
         <h2 class="listing-heading">Step {{thumbnailDetails.step}}</h2>
         <p class="listing-location">{{thumbnailDetails.details}}</p>
